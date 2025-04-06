c6079ff060fa3d4f12f2e8072b3d9893eded19ac
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-04-06 17:03:58 +0530

    updated create city api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,31 +1,26 @@
-const {StatusCodes} = require('http-status-codes')
-const { CityService } =  require('../services');
-const {SuccessResponse, ErrorResponse} = require('../utils/common')
-
-// POST: /cities
-// req-body {name: 'London'}
-console.log("inside city controller");
-
-
+const { StatusCodes } = require('http-status-codes');
+const { CityService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+/**
+ * POST : /cities 
+ * req-body {name: 'London'}
+ */
 async function createCity(req, res) {
-    
-    try {
+    try {        
         const city = await CityService.createCity({
-            name: req.body.name,            
-        })
+            name: req.body.name
+        });
         SuccessResponse.data = city;
         return res
-            .status(StatusCodes.CREATED)
-            .json(SuccessResponse)
-    } catch (error) {
-        
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse);
+    } catch(error) {
         ErrorResponse.error = error;
         return res
-            .status(error.statusCode)
-            .json(ErrorResponse)
+                .status(error.statusCode)
+                .json(ErrorResponse);
     }
 }
-
 module.exports = {
-    createCity,
+    createCity
 }
